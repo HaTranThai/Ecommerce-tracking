0d15ae6156d0f1e633afc3a6ca84d5b9c361b7f7
TranHa <ha20040204@gmail.com>
2025-07-02 16:52:42 +0700

    update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@
 URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,18 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-from rest_framework_simplejwt.views import TokenRefreshView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.api.urls')),
-    path('api/product/', include('product.api.urls')),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/order/', include('order.api.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/order/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import purchase_single_product, purchase_cart_items, OrderHistoryView
-
-urlpatterns = [
-    path('purchase/single/', purchase_single_product),
-    path('purchase/cart/', purchase_cart_items),
-    path('history/', OrderHistoryView.as_view(), name='order-history'),
-]
